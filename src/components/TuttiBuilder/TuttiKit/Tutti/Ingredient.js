aa54e6173534b578ba026c0b49ab09a49b9d61a5
azimcarybaev <azimcarubaev.15>
2020-10-23 13:36:34 +0600

    code

--- src/components/TuttiBuilder/TuttiKit/Tutti/Ingredient.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import classes from "./Ingredient.module.css";
-
-export default ({ type }) => {
-  const IngredientClasses = [classes.Ingredient, classes[type]];
-
-  return <div className={IngredientClasses.join(" ")}></div>;
-};
--- src/components/TuttiBuilder/TuttiKit/TuttiKit.js
+++ src/components/TuttiBuilder/TuttiKit/TuttiKit.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Ingredient from "./Ingredient/Ingredient";
+import Tutti from "./Tutti/Tutti";
 import classes from "./TuttiKit.module.css";
 
 export default ({ price, ingredients }) => {
@@ -7,14 +7,16 @@ export default ({ price, ingredients }) => {
 
   Object.keys(ingredients).forEach((ingredient) => {
     for (let i = 0; i < ingredients[ingredient].quantity; i++) {
-      ingredientsOutput.push(<Ingredient key={ingredient + i} type={ingredient} />);
+      ingredientsOutput.push(<Tutti key={ingredient + i} type={ingredient} />);
     }
   });
 
   return (
     <div className={classes.TuttiKit}>
-      <div className={classes.bento}>{ingredientsOutput}</div>
-      <div className={classes.price}>{price.toFixed(2)} som</div>
+      <div className={classes.bento}>
+        <div className={classes.Tuttu}>{ingredientsOutput}</div>
+      </div>
+      <div className={classes.price}> {price} som</div>
     </div>
   );
 };
